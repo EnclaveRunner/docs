7c5bf1918164531d6aa3d14dc8e2ee9c4e0c9593
kosinamatteo <matteokosina@icloud.com>
2025-08-02 22:19:15 -0700

    chore: remove unused comments and clean up a bit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -34,24 +34,12 @@ const config: Config = {
   // may want to replace "en" with "zh-Hans".
 
   themes: [
-    // ... Your other themes.
     [
       require.resolve("@easyops-cn/docusaurus-search-local"),
       /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
       {
-        // ... Your options.
         // `hashed` is recommended as long-term-cache of index file is possible.
         hashed: true,
-
-        // For Docs using Chinese, it is recomended to set:
-        // language: ["en", "zh"],
-
-        // Customize the keyboard shortcut to focus search bar (default is "mod+k"):
-        // searchBarShortcutKeymap: "s", // Use 'S' key
-        // searchBarShortcutKeymap: "ctrl+shift+f", // Use Ctrl+Shift+F
-
-        // If you're using `noIndex: true`, set `forceIgnoreNoIndex` to enable local index:
-        // forceIgnoreNoIndex: true,
       },
     ],
   ],
@@ -62,8 +50,6 @@ const config: Config = {
       {
         docs: {
           sidebarPath: "./sidebars.ts",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/EclaveRunner/docs/blob/main",
         },
         blog: false,
@@ -75,8 +61,7 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/enclave_color.svg",
     navbar: {
       title: "Enclave",
       logo: {
