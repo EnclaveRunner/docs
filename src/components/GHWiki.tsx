1386dcdd3b530179dcde950cd790f826bff206fa
matteokosina <matteokosina@icloud.com>
2025-10-21 15:18:08 +0200

    cleanup

--- src/components/GHWiki.tsx
+++ src/components/GHWiki.tsx
@@ -1,36 +1,35 @@
 import { useEffect, useState } from "react";
-import Markdown from 'react-markdown';
+import Markdown from "react-markdown";
 
-export function GHWiki({wikiUrl }: { wikiUrl: string }) {
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
-    const [wikiContent, setWikiContent] = useState<string | null>(null);
+export function GHWiki({ wikiUrl }: { wikiUrl: string }) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [wikiContent, setWikiContent] = useState<string | null>(null);
 
-     useEffect(() => {
-        fetch(wikiUrl)
-          .then((res) => {
-            if (!res.ok) {
-              throw new Error(
-                `Failed to fetch API docs from ${wikiUrl} (status: ${res.status} ${res.statusText})`
-                );
-            }
-            
-           
-            return res.text();
-          })
-          .then(setWikiContent)
-          .catch((e) => {
-            setError(`Error fetching API docs from ${wikiUrl}: ${e.message}`);
-          })
-          .finally(() => setLoading(false));
-      }, [wikiUrl]);
-      if (loading) return <div>Loading Wiki documentation...</div>;
-      if (error) return <div className="alert alert--danger">Error: {error}</div>;
+  useEffect(() => {
+    fetch(wikiUrl)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(
+            `Failed to fetch API docs from ${wikiUrl} (status: ${res.status} ${res.statusText})`
+          );
+        }
 
-    return (
-        <> 
-            <Markdown>{wikiContent}</Markdown>
-        </>
-    );
-    }
-    export default GHWiki;
+        return res.text();
+      })
+      .then(setWikiContent)
+      .catch((e) => {
+        setError(`Error fetching API docs from ${wikiUrl}: ${e.message}`);
+      })
+      .finally(() => setLoading(false));
+  }, [wikiUrl]);
+  if (loading) return <div>Loading Wiki documentation...</div>;
+  if (error) return <div className="alert alert--danger">Error: {error}</div>;
+
+  return (
+    <>
+      <Markdown>{wikiContent}</Markdown>
+    </>
+  );
+}
+export default GHWiki;
