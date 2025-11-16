dda0165bbe098c09c826d71a81e75ed7be400e91
matteokosina <matteokosina@icloud.com>
2025-11-16 23:59:36 +0100

    fix: typo in sidebar import

--- sidebars.ts
+++ sidebars.ts
@@ -1,5 +1,5 @@
 import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
-import apiSidebar from "./docs/API/sidebar";
+import apiSidebar from "./docs/api/sidebar";
 
 /**
  * Creating a sidebar enables you to:
